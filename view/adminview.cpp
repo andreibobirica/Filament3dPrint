3fa8bda996255ba0497f4fa1b1bdd4c7e60e37c3
Andrei <andreibobirica99@gmail.com>
2022-01-18 14:03:17 +0100

    Prova di un Commit ERRATO

--- view/adminview.cpp
+++ view/adminview.cpp
@@ -14,7 +14,7 @@ AdminView::AdminView(QSize* s,View* parent) :
     mainLayout->addWidget(new QPushButton("Home"),0,13,1,1,Qt::AlignRight);
 
     //Material Table
-    QTableWidget* materialTable = new QTableWidget();
+    QTableWidgethhh* materialTable = new QTableWidget();
     materialTable->setRowCount(10);
     materialTable->setColumnCount(2);
     QStringList headersMaterialTable = { "Materiale", ""};
